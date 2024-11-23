8a7a011a4d52ecbb81cfa87e255089037169add9
dinesh karthik <141578851+Dineshkarthik1@users.noreply.github.com>
2024-11-23 10:53:32 +0000

    added data

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data analyst',
+    'location': 'delhi',
+    'salary':'rs.100000'
+  },
+  {
+    'id': 2,
+    'title':'Data scientist',
+    'location': 'hyderabad',
+    'salary': 'rs.150000'
+  }
+]
+
 @app.route("/")
 def home():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name = 'Sosuke Aizen ')
 
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(
